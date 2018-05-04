125c82c6f2e9dc866debb73be8cb0421dff84b35
JokerLiu <liuyuelin0428@163.com>
2018-05-04 10:38:47 +0800

    add CircularLinkedList

---
+++ src/com/datastructures/CircularLinkedList.java
@@ -0,0 +1,57 @@
+package com.datastructures;
+
+/**
+ * 循环链表
+ *
+ * @package com.datastructures
+ * @auther joker
+ * @date 2018/5/4 10:18
+ */
+public class CircularLinkedList {
+    private int length;
+    private CircularLinkedList next;
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public CircularLinkedList getNext() {
+        return next;
+    }
+
+    public void setNext(CircularLinkedList next) {
+        this.next = next;
+    }
+
+    int getCLLLength(CircularLinkedList cll) {
+        int length = 0;
+        CircularLinkedList currentNode = cll;
+        while (currentNode != null) {
+            length++;
+            currentNode = currentNode.getNext();
+            if (cll == currentNode) {// 判断当前节点是否遍历到头节点
+//                return length;
+                break;
+            }
+        }
+        return length;
+    }
+
+    public static void main(String[] args) {
+        CircularLinkedList cll1 = new CircularLinkedList();
+        CircularLinkedList cll2 = new CircularLinkedList();
+        CircularLinkedList cll3 = new CircularLinkedList();
+        CircularLinkedList cll4 = new CircularLinkedList();
+
+        cll1.setNext(cll4);
+//        cll2.setNext(cll3);
+//        cll3.setNext(cll4);
+        cll4.setNext(cll1);
+
+        System.out.println(cll1.getCLLLength(cll1));
+    }
+}
--- src/com/datastructures/DoubleLinkedList.java
+++ src/com/datastructures/DoubleLinkedList.java
@@ -65,7 +65,6 @@ public class DoubleLinkedList {
                     }
                 }
             }
-
         }
 
         return length;
